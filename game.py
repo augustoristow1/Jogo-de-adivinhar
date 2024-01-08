a2d7fccbd3fc4b559d0e405b8b84356912d3c724
augustoristow1 <augustoisantos@hotmail.com>
2024-01-08 17:30:49 -0300

    Update game.py

--- game.py
+++ game.py
@@ -10,7 +10,17 @@ chute = int(chute_string)
 
 print("Você digitou", chute_string)
 
-if(numero_secreto == chute):
+acertou = numero_secreto == chute;
+maior = chute > numero_secreto;
+menor = chute < numero_secreto;
+
+if(acertou):
     print("Você acertou!")
 else:
-    print("Você errou!"); 
+    if (maior):
+        print("Você errou! Seu chute é maior que o número secreto")
+    elif (menor):
+        print("Você errou! Seu chute é menor que o número secreto")    
+print("Fim de jogo"); 
+
+
