9ba31d914b0a6f9dbc301832311b39e0b0806702
augustoristow1 <augustoisantos@hotmail.com>
2024-02-22 18:56:27 -0300

    Update.1 game.py

--- game.py
+++ game.py
@@ -1,26 +1,42 @@
-print("***********************************")
-print("Bem vindo ao jogo de adivinhação!")
-print("***********************************")
-
+print ("***********************************")
+print(" Bem vindo ao jogo de adivinhar ")
+print  ("***********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3 # usando range para ficar com 3
 
-chute_string = input("Digite o seu número:")
-chute = int(chute_string)
+                                                #O último numero da rodada é nulo, considerado o final, então aumente +1
+                                                # a função range é usada para fazer um LAÇO
+for rodada in range(1, total_de_tentativas + 1):         # range define a serie de valores
+    print("Tentativa, {} de {}" .format( rodada, total_de_tentativas))
 
-print("Você digitou", chute_string)
+    chute_str = input(" Digite o seu numero:")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-acertou = numero_secreto == chute;
-maior = chute > numero_secreto;
-menor = chute < numero_secreto;
+                                    #String Intrepolation ( .format ) 'e usado para formatar
+                                    # {} é usado para representar um valor/numeração
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if (maior):
-        print("Você errou! Seu chute é maior que o número secreto")
-    elif (menor):
-        print("Você errou! Seu chute é menor que o número secreto")    
-print("Fim de jogo"); 
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
+    if(acertou):
+        print("Você acertou!") #caso acerte de primeira usamos "break" para para o código
+        break 
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto")
+
+
+
+    
+print("Fim de jogo")
+
+
+
+
+# para função de loop/laço usar "For ... range"
 
